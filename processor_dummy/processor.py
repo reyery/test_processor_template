5ab444f7864ffa55befb82783e6c249ccf52ad04
Reynold Mok <me@reynoldm.com>
2021-05-10 09:35:48 +0800

    Remove return statements

--- processor_dummy/processor.py
+++ processor_dummy/processor.py
@@ -7,34 +7,27 @@ logger = logging.getLogger('testing.dummy_adapter')
 
 
 def function(working_directory):
-    try:
-        a_path = os.path.join(working_directory, 'a')
-        with open(a_path, 'r') as f:
-            a = json.load(f)
-            a = a['v']
+    a_path = os.path.join(working_directory, 'a')
+    with open(a_path, 'r') as f:
+        a = json.load(f)
+        a = a['v']
 
-        b_path = os.path.join(working_directory, 'b')
-        with open(b_path, 'r') as f:
-            b = json.load(f)
-            b = b['v']
+    b_path = os.path.join(working_directory, 'b')
+    with open(b_path, 'r') as f:
+        b = json.load(f)
+        b = b['v']
 
-        print(f"a={a}")
-        print(f"b={b}")
-        c = {
-            'v': a + b
-        }
-        print(f"c={c}")
+    print(f"a={a}")
+    print(f"b={b}")
+    c = {
+        'v': a + b
+    }
+    print(f"c={c}")
 
-        c_path = os.path.join(working_directory, 'c')
+    c_path = os.path.join(working_directory, 'c')
 
-        with open(c_path, 'w') as f:
-            json.dump(c, f, indent=4, sort_keys=True)
-
-        return True
-
-    except Exception as e:
-        logger.error(f"exception in function: {e}")
-        return False
+    with open(c_path, 'w') as f:
+        json.dump(c, f, indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
